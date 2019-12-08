54be344ed2a0941dc4820759bf53ea35b1ecf375
Mariusz Masztalerczuk <mariusz@masztalerczuk.com>
2019-12-08 13:25:11 +0100

    WIP

--- lib/repositories/users/user_repository.dart
+++ lib/repositories/users/user_repository.dart
@@ -1,24 +1,34 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
+import 'package:questlly/common/common.dart';
+import 'package:questlly/common/exceptions.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserRepository {
+  final String apiUrl = DotEnv().env['API_URL'];
+
   Future<String> authenticate({
     @required String username,
     @required String password,
   }) async {
-    var url = 'https://3e2f05b8.ngrok.io/api-token-auth/';
-    var response = await http.post(url,
-        body: {"username": username, "email": "", "password": password});
+    var url = apiUrl + '/api-token-auth/';
+    var responseJson;
 
-    print(response.body);
+    try {
+      final response = await http.post(url,
+          body: {"username": username, "email": "", "password": password});
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet connection');
+    }
 
-    Map response_map = jsonDecode(response.body);
-    persistToken(response_map['token']);
+    persistToken(responseJson['token']);
 
-    return response_map['token'];
+    return responseJson['token'];
   }
 
   Future<String> register({
@@ -26,12 +36,14 @@ class UserRepository {
     @required String password,
     @required String email,
   }) async {
-    var url = 'https://3e2f05b8.ngrok.io/api/v1/rest-auth/registration/';
-    var response = await http.post(url,
-        body: {"username": username,
-               "email": email,
-               "password1": password,
-               "password2": password});
+    var url = DotEnv().env['API_URL'] + '/api/v1/rest-auth/registration/';
+
+    var response = await http.post(url, body: {
+      "username": username,
+      "email": email,
+      "password1": password,
+      "password2": password
+    });
 
     print(response.body);
 
@@ -55,7 +67,6 @@ class UserRepository {
 
   Future<bool> hasToken() async {
     final prefs = await SharedPreferences.getInstance();
-
 
     final token = prefs.getString('token') ?? 0;
     return false;
--- test/repositories/users/user_repository_test.dart
+++ test/repositories/users/user_repository_test.dart
@@ -1,7 +1,16 @@
 // Import the test package and Counter class
 import 'package:flutter_test/flutter_test.dart';
+import 'package:questlly/repositories/users/user_repository.dart';
 
 
 void main() {
   test('Empty test', () {});
+  test('UserRepository authenticate', () {
+    final userRepository = UserRepository();
+    final username = 'test_login';
+    final password = 'password_login';
+
+    final responseJson =
+      userRepository.authenticate(username: username, password: password);
+  });
 }
